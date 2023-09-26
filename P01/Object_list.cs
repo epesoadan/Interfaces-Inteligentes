34ceb045aecffe639786fc4e7a41e1ba2767885a
Eva Peso Adán <alu0101398037@ull.edu.es>
2023-09-26 21:05:58 +0100

    Add files via upload

---
+++ P01/Object_list.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_list : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The objects used were:");
+        Debug.Log("- Plant (from Terrain Starter Pack)");
+        Debug.Log("- Player Armature (from Third Person Starter Pack)");
+        Debug.Log("- Boxes (from Third Person Starter Pack)");
+        Debug.Log("- Box (Basic 3D object)");
+        Debug.Log("- Circle (Basic 3D object)");
+        Debug.Log("- Capsule (Basic 3D object)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
