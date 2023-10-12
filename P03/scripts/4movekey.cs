66c9bebd60c2a1518186eeccd4e8876b9a48ea39
Eva Peso Adán <alu0101398037@ull.edu.es>
2023-10-12 21:57:13 +0100

    Add files via upload

---
+++ P03/scripts/4movekey.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movekey : MonoBehaviour
+{
+    private GameObject cube;
+    private GameObject sphere;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = gameObject;
+        sphere = GameObject.FindWithTag("main_sphere");
+        speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector3 moveCube = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        cube.transform.Translate(moveCube * speed, 0);
+
+        Vector3 moveSphere = new Vector3(Input.GetAxis("HorizontalSp"), 0, Input.GetAxis("VerticalSp"));
+        sphere.transform.Translate(moveSphere * speed, 0);   
+    }
+}
