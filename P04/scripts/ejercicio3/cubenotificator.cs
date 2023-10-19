0b4c9cfe0cd1a74cdaf9a5a89b85b9363ee980b0
Eva Peso Adán <alu0101398037@ull.edu.es>
2023-10-20 00:34:07 +0100

    Update cubenotificator.cs

--- P04/scripts/ejercicio3/cubenotificator.cs
+++ P04/scripts/ejercicio3/cubenotificator.cs
@@ -6,7 +6,6 @@ public class cubenotificator : MonoBehaviour
 {
     public delegate void MyEvent();
     public event MyEvent OnGettingCloserToCylinder;
-    //public float distancia_minima = 0.16f; // Distancia que hay que recorrer para que se considere que se ha movido
     private float previous_distance = 0f;
 
     // Start is called before the first frame update
